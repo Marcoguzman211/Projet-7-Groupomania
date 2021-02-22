a3936fca0a7b014f898ce382a68de87c521d5a4d
Marco Guzman <62027255+Marcoguzman211@users.noreply.github.com>
2021-02-22 22:24:56 +0100

    Serveur avec mysql

--- Backend/controllers/users.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const connection = require('../connection')
-
-//Inscription de l'Utilisateur
-exports.signup = (req, res, next) => {
-    const user = req.body
-    connection.query()
-}
--- Backend/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const usersCtlr = require('../controllers/users')
-
-
-router.post('/signup', usersCtlr.signup);
-
-module.exports = router
