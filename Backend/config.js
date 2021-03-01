54f4e6a38905f008640ba95a5b77363b281a2ef7
Marco Guzman <62027255+Marcoguzman211@users.noreply.github.com>
2021-03-01 16:58:43 +0100

    Connexion mySQL errors

--- Backend/config.js
+++ Backend/config.js
@@ -3,9 +3,9 @@ dotenv.config()
 
 module.exports = {
     mysql_database: {
-        host: "localhost",
-        user: 'groupomania',
-        password: 'OcrAccess7',
-        database: 'groupodb'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     }
 }
--- Backend/connection.js
+++ Backend/connection.js
@@ -1,18 +1,14 @@
-const mysql = require('mysql'); // importation du paquet mysql
+const mysql = require('mysql')
+const { mysql_database } = require('./config')
+const { connect } = require('./routes/publications')
 
-const db = mysql.createPool({ // création d'une pool de connexion à la base de données             
-    connectionLimit: 30, // nombre maximum de création de connexion avec la base de données
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const connection = mysql.createConnection(mysql_database)
 
-db.query('SELECT 1 + 1 AS solution', function(error, results, fields) { // Test de la connexion avec la base de données
-    if (error) {
-        return console.error('error: ' + error.message); // Erreur de connexion
+connection.connect((err, conn) => {
+    if (err) {
+        console.error('error: ' + err.message)
+    } else {
+        console.log('Connexion à la base des données MySQL réussie !')
+        return conn
     }
-    console.log("Connexion à la base de données MySQL validée !"); // Connexion validée
-});
-
-module.exports = db;
+})
